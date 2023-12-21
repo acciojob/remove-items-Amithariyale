4ce6c42e7f0fd74ed7563c9d1b7a85183a4a45ba
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-21 19:37:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+const btn=document.querySelector(`form>input`);
+const select=document.getElementById('colorSelect');
+const options=document.getElementsByTagName('option');
+
+btn.addEventListener('click',()=>{
+	let value=select.value;
+	for(let i=0;i<options.length;i++){
+		console.log(value);
+		console.log(options[i].value)
+		if(options[i].value===value){
+			options[i].remove();
+			break;
+		}
+	}
+})
+
